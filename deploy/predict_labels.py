d4ba8eaed33446093b9149fad1da09aa49954cbf
韩宏炜 <hanhongwei@hanhongweideMacBook-Pro.local>
2023-02-18 22:16:07 +0800

    slight

--- deploy/predict_labels.py
+++ deploy/predict_labels.py
@@ -9,10 +9,9 @@ sys.path.append('../code/src/')
 from models import ClassificationModel, CEModel, FeatEnc
 from excel_toolkit import get_sheet_names, get_sheet_tarr, get_feature_array
 
-def main(fname, ce_model_path, fe_model_path, cl_model_path, w2v_path, vocab_size, infersent_source, infersent_model):
+def load(ce_model_path, fe_model_path, cl_model_path, w2v_path, vocab_size, infersent_source, infersent_model):
     sys.path.append(infersent_source)
     from helpers import Preprocess, SentEnc, label2ind
-    from test_cl import predict_labels
 
     mode = 'ce+f'
     device = 'cpu'
@@ -24,31 +23,35 @@ def main(fname, ce_model_path, fe_model_path, cl_model_path, w2v_path, vocab_siz
     n_classes = 6
     if device != 'cpu': torch.cuda.set_device(device)
 
-    ce_model = CEModel(senc_dim, ce_dim//2, window*4)
+    ce_model = CEModel(senc_dim, ce_dim // 2, window * 4)
     ce_model = ce_model.to(device)
     fe_model = FeatEnc(f_dim, fenc_dim)
     fe_model = fe_model.to(device)
-    cl_model = ClassificationModel(ce_dim+fenc_dim, n_classes).to(device)
+    cl_model = ClassificationModel(ce_dim + fenc_dim, n_classes).to(device)
 
     ce_model.load_state_dict(torch.load(ce_model_path, map_location=device))
     fe_model.load_state_dict(torch.load(fe_model_path, map_location=device))
     cl_model.load_state_dict(torch.load(cl_model_path, map_location=device))
 
     label2ind = ['attributes', 'data', 'header', 'metadata', 'derived', 'notes']
-    
+
     print('loading word vectors...')
-    senc = SentEnc(infersent_model, w2v_path, 
+    senc = SentEnc(infersent_model, w2v_path,
                    vocab_size, device=device, hp=False)
     prep = Preprocess()
-    
+    return senc,cl_model, ce_model, fe_model, senc, mode, device, label2ind
+
+def process(fname,models):
+    from test_cl import predict_labels
+    senc,cl_model, ce_model, fe_model, senc, mode, device, label2ind=models
     snames = get_sheet_names(fname, file_type='xls')
-    
+
     result = dict()
     for sname in snames:
         tarr, n, m = get_sheet_tarr(fname, snames[0], file_type='xls')
         ftarr = get_feature_array(fname, snames[0], file_type='xls')
         table = dict(table_array=tarr, feature_array=ftarr)
-        
+
         sentences = set()
         for row in tarr:
             for c in row:
@@ -59,6 +62,11 @@ def main(fname, ce_model_path, fe_model_path, cl_model_path, w2v_path, vocab_siz
         probs = np.exp(probs)
         labels = np.vectorize(lambda x: label2ind[x])(labels)
         result[sname] = dict(text=tarr.tolist(), labels=labels.tolist(), labels_probs=probs.tolist())
+    return result
+
+def main(fname, ce_model_path, fe_model_path, cl_model_path, w2v_path, vocab_size, infersent_source, infersent_model):
+    models=load(ce_model_path, fe_model_path, cl_model_path, w2v_path, vocab_size, infersent_source, infersent_model)
+    result=process(fname,models)
     return result
 
 if __name__=='__main__':
